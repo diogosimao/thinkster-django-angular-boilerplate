1519a8b3361cff4aa6255237558394041ba1e721
diogosimao <diogosimao@gmail.com>
2017-07-31 02:45:33 -0300

    Checkpoint: Register a new user at http://localhost:8000/register and confirm the Account object was created

---
+++ authentication/migrations/0002_auto_20170731_0255.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='account',
+            old_name='create_at',
+            new_name='created_at',
+        ),
+        migrations.RenameField(
+            model_name='account',
+            old_name='update_at',
+            new_name='updated_at',
+        ),
+    ]
