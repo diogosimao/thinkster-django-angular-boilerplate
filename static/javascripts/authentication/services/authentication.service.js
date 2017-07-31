ab208c8cbaa1278ee19e310e61de16f22a1525e1
diogosimao <diogosimao@gmail.com>
2017-07-31 17:05:33 -0300

    Checkpoint: Make a controller called LoginController in static/javascripts/authentication/controllers/login.controller.js

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -9,6 +9,11 @@
 
     function Authentication($cookies, $http) {
         var Authentication = {
+            unauthenticate: unauthenticate,
+            setAuthenticatedAccount: setAuthenticatedAccount,
+            isAuthenticated: isAuthenticated,
+            getAuthenticatedAccount: getAuthenticatedAccount,
+            login: login,
             register: register
         };
         return Authentication;
@@ -19,6 +24,42 @@
                 password: password,
                 email: email
             });
+        }
+
+        function login(email, password) {
+            return $http.post('/api/v1/auth/login', {
+                email: email, password: password
+            }).then(loginSuccessFn, loginErrorFn);
+            
+            function loginSuccessFn(data, status, headers, config){
+                Authentication.setAuthenticatedAccount(data.data);
+                
+                window.location = '/';
+            }
+            
+            function loginErrorFn(data, status, hearders, config){
+                console.error('Falha épica!');
+            }
+        }
+
+        function getAuthenticatedAccount() {
+            if (!$cookies.authenticatedAccount){
+                return;
+            }
+
+            return JSON.parse($cookies.authenticatedAccount);
+        }
+
+        function isAuthenticated(){
+            return !!$cookies.authenticatedAccount;
+        }
+
+        function setAuthenticatedAccount(account){
+            $cookies.authenticatedAccount = JSON.stringfy(account);
+        }
+
+        function unauthenticate(){
+            delete $cookies.authenticatedAccount;
         }
     }
 })();
