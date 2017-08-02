8133ac9a3876551e55d9c40ddc1f8562ce1942af
diogosimao <diogosimao@gmail.com>
2017-08-01 23:23:10 -0300

    Checkpoint Play around with the Post model and PostSerializer serializer in Django's shell

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -6,13 +6,23 @@ from rest_framework_nested import routers
 
 from authentication.views import AccountViewSet, LoginView, LogoutView
 
+from posts.views import AccountPostsViewSet, PostViewSet
+
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostViewSet)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = patterns(
     '',
 
     url(r'^api/v1/', include(router.urls)),
+
+    url(r'^api/v1/', include(router.accounts_router.urls)),
 
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 
